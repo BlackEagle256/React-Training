3fab06d6bb4fe75282db0d432b7a4a493b489697
MohammadHosein <mohammadhoseindadgostr@gmail.com>
2024-08-02 11:56:16 +0330

    Adding First And Second Mini Project React

---
+++ second/src/App.js
@@ -0,0 +1,26 @@
+
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <div className="Nav-Back">
+        <div className="Nav-Container">
+          <h1><a href="#">MohammadHosein MH</a></h1>
+        </div>
+
+        <nav>
+          <ul className="nav-ul">
+            <li className="Nav-Link active-link">Home</li>
+            <li className="Nav-Link">News</li>
+            <li className="Nav-Link">About</li>
+            <li className="Nav-Link">Contact</li>
+          </ul>
+        </nav>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
